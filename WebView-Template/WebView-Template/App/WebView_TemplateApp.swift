235cb31e5f4c62b9049f2c88ca878bcf08fa005d
austriaInEurope <christine16lav@mail.ru>
2026-04-01 15:58:47 +0300

    SwiftUI WebView Template для iOS
    Готовый шаблон iOS-приложения на **SwiftUI + WKWebView** для быстрой интеграции веб-сайта в нативное приложение.
    Проект демонстрирует практические навыки разработки iOS-приложений с акцентом на чистую архитектуру, качественный UX и готовность к публикации.
    
    ## О проекте
    
    Данный шаблон предназначен для быстрого запуска iOS-приложений на основе уже существующего веб-сайта.
    
    Подходит для:
    - интернет-магазинов
    - ресторанов и кафе
    - сервисов бронирования
    - веб-платформ
    - лендингов
    - личных кабинетов
    
    Проект легко адаптируется под любой сайт за счёт смены URL и визуальных ресурсов.
    
    ## Основной функционал
    - интеграция сайта через `WKWebView`
    - splash screen при первом запуске
    - экран загрузки
    - кастомный loader для плавных переходов
    - устранение белого экрана при навигации
    - отслеживание интернет-соединения
    - экран отсутствия сети
    - обработка внешних ссылок (например Telegram)
    - поддержка deep links
    - готовность к TestFlight / App Store
    
    ### Описание структуры (MVVM)
    
    **App**
    - точка входа приложения
    - жизненный цикл
    - инициализация сервисов
    
    **Presentation**
    - пользовательский интерфейс
    - экраны приложения
    - компоненты
    - loader / splash screen
    
    **Services**
    - мониторинг сети
    - deep links
    - сервисные классы
    
    **Resources**
    - Assets
    - изображения
    - иконки
    - splash logo
    
    ## Используемые технологии
    - Swift
    - SwiftUI
    - WKWebView
    - MVVM
    - Combine
    - Network Framework
    
    ##Автор
    
    Кристина Лаврикова  
    iOS Developer (Swift / SwiftUI)

---
+++ WebView-Template/WebView-Template/App/WebView_TemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebView_TemplateApp.swift
+//  WebView-Template
+//
+//  Created by Кристина Игоревна on 01.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct WebView_TemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
